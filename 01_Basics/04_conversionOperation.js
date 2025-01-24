4f4b60bb4ffad649799afe1679085e08f0ea1d17
Pravin Chavan <159113280+Pravinchavan24@users.noreply.github.com>
2025-01-24 21:06:13 +0530

    update conversionOperation

---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,43 @@
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+// -3
+
+// 4
+// 0
+// 4
+// 4
+// 1
+// 0
+
+
+let str1 = "Pravin";
+let str2 = " Chavan";
+let str3 = str1 + str2;
+console.log(str3);
+
+//   Pravin Chavan
+
+
+console.log((3 + 4 ) * 3 % 4);
+
+// 1
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+// true 
+// 1
+// 0
+
+
