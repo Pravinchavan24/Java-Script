ff1a94e311bb2e93c3fbaecb660975c6f525ad39
Pravin Chavan <159113280+Pravinchavan24@users.noreply.github.com>
2025-01-25 20:38:40 +0530

    create comparison code

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,32 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(3 <= 4);
+console.log(2 == 1);
+console.log(2 != 1);
+// true
+// false
+// true
+// false
+// true
+
+console.log("2" > 1);
+console.log("20" < 1);
+// true 
+// false
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+// false
+// false
+// false
+// true
+// true
+
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+
