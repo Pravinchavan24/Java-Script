00cd245d659a7e99141ea2bd41ce183d1545093c
Pravin Chavan <159113280+Pravinchavan24@users.noreply.github.com>
2025-02-03 21:20:18 +0530

    update datatype sammary in stack and heap code

---
+++ 01_Basics/datatypesSammary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 Type : String, Number, Boolearn, null, undefined, BigInt, Symbol.
+
+
+
+// Non-Primitive 
+// Array, Object, Functions
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "P24 Gameplay"
+
+let anothernmae = myYoutubename
+anothernmae = "P24 Gameplay kids"
+
+console.log(myYoutubename);
+console.log(anothernmae);
+
+let userOne = {
+    email: "user@gamil.com",
+    upi: "user@ybl";
+}
+
+let userTwo = userOne
+
+userTwo.email = "Pravinchavan@gamil.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
